885477666794dd7674e9c8cab5210721572887fa
Pranab Kalita <pavankalita@gmail.com>
2024-10-15 13:25:28 +0530

    Updated Pagination to include total records

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -7,15 +7,19 @@
 </script>
 
 <template>
-    <nav class="relative flex justify-center">
-        <template v-for="link in pagination.links" :key="link.label">
-            <Link
-                preserve-scroll
-                :href="link.url ?? ''"
-                v-html="link.label"
-                class="flex items-center justify-center px-3 py-2 text-sm rounded-lg text-gray-600"
-                :class="{ 'bg-gray-200': link.active, '!text-gray-300': !link.url }"
-            />
-        </template>
-    </nav>
+    <div class="flex justify-between">
+        <p class="flex items-center justify-center px-3 py-2 text-sm rounded-lg text-gray-600">Total: {{ pagination.total }}</p>
+
+        <nav class="relative flex justify-center">
+            <div v-for="link in pagination.links" :key="link.label">
+                <Link
+                    preserve-scroll
+                    :href="link.url ?? ''"
+                    v-html="link.label"
+                    class="flex items-center justify-center px-3 py-2 text-sm rounded-lg text-gray-600"
+                    :class="{ 'bg-gray-200': link.active, '!text-gray-300': !link.url }"
+                />
+            </div>
+        </nav>
+    </div>
 </template>
